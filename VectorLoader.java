d79c8c7e9903c91a5a8cb4ff8089344f93b9ec9d
Lukegogogo <lq2145@columbia.edu>
2014-12-22 12:43:06 -0500

    Update VectorLoader.java

--- VectorLoader.java
+++ VectorLoader.java
@@ -142,7 +142,7 @@ public class App
 	}
 	public static void fileWriter(String fileName, String content) {  
         try {  
-            //打开一个写文件器，构造函数中的第二个参数true表示以追加形式写文件  
+         
             FileWriter writer = new FileWriter(fileName, true);  
             writer.write(content);  
             writer.close();  
